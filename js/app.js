e25a7e864081ac9eeda4aab436867609f54bcea6
Christina Eckleder <christina.eckleder.ext@pentasys.de>
2014-11-25 21:12:29 +0100

    created enemy and player

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+
+// General helper methodes and global variables
+var randomNumber = function(lowerBound, upperBound) {
+    return Math.floor(Math.random() * (upperBound - lowerBound + 1)) + lowerBound;
+}
+// Global gabe state variable
+var gameState = true;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = -200;
+    this.y = (randomNumber(1, 3) * 83) - 20;
+    this.speed = randomNumber(20, 150);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +25,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > canvas.width){
+        this.x = - Resources.get(this.sprite).width;
+    }
+
+    this.x = this.x + this.speed * dt;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +40,86 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 2 * 101;
+    this.y = 5 * 83 - 10;
+    this.lives = 3;
+    this.score = 0;
+    this.sprite = 'images/char-cat-girl.png';
+}
+
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    var heartX = canvas.width - 30;
+    for (i = 0; i < player.lives; i++) {
+        ctx.drawImage(Resources.get(SPRITES['heart']), heartX, canvas.height - 60, 30, 40);
+        heartX -= 35;
+
+    }
+}
+
+Player.prototype.reduceLife = function() {
+    this.lives -= 1;
+    console.log(this.lives);
+}
+
+Player.prototype.reset = function() {
+    player.lives = 3;
+    this.x = 2 * 101;
+    this.y = 5 * 83 - 10;
+
+}
+
+
+Player.prototype.handleInput = function(direction) {
+    switch (direction) {
+        case 'left':
+            if (this.x >= 101) {
+                this.x -= 101;
+            }
+            break;
+        case 'right':
+            if (this.x < canvas.width - 101) {
+                this.x += 101
+            }
+            break;
+        case 'up':
+            if (this.y > 0) {
+                this.y -= 83;
+            }
+            break;
+        case 'down':
+            if (this.y < (83 * 5 - 10)) {
+                this.y += 83;
+            }
+            break;
+        case 'space':
+            if (gameState == false) {
+                player.reset();
+                gameState = true;
+            }
+            console.log(gameState);
+            break;
+    }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var allEnemies = [];
+for (i = 0; i < 2; i++) {
+    var enemy = new Enemy();
+    allEnemies.push(enemy);
+}
+
+var player = new Player();
+
 
 
 
@@ -39,7 +130,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        32: 'space'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,17 @@
 var Engine = (function(global) {
+    var SPRITES = {
+    'boy' : 'images/char-boy.png',
+    'cat-girl': 'images/char-cat-girl.png',
+    'water': 'images/water-block.png',
+    'stone': 'images/stone-block.png',
+    'block': 'images/stone-block.png',
+    'grass': 'images/grass-block.png',
+    'bug': 'images/enemy-bug.png',
+    'heart': 'images/Heart.png'
+    };
+    var background = ['water', 'stone', 'stone', 'stone', 'grass', 'grass']
+
+
     var doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
@@ -11,18 +24,17 @@ var Engine = (function(global) {
     doc.body.appendChild(canvas);
 
     function main() {
-        var now = Date.now(),
+        if (gameState == true) {
+            var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
-
-        update(dt);
-        render();
-
-        lastTime = now;
+            update(dt);
+            render();
+            lastTime = now;
+        }
         win.requestAnimationFrame(main);
     };
 
     function init() {
-
         reset();
         lastTime = Date.now();
         main();
@@ -30,7 +42,8 @@ var Engine = (function(global) {
 
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
+        checkBackground();
     }
 
     function updateEntities(dt) {
@@ -40,26 +53,54 @@ var Engine = (function(global) {
         player.update();
     }
 
+    function checkCollisions() {
+        allEnemies.forEach(function(enemy) {
+            if (enemy.x + 80 > player.x && enemy.x < (player.x + 101)) {
+                if ((enemy.y + 20) == (player.y + 10)) {
+                    player.reduceLife();
+                    checkGameState();
+                }
+            }
+        });
+    }
+
+    function checkBackground() {
+        var playerRow = (player.y + 10) / 83,
+        currentBackground = background[playerRow];
+        if (currentBackground == 'water') {
+            player.reduceLife();
+            checkGameState();
+        }
+
+    }
+
+    function checkGameState() {
+        if (player.lives > 0) {
+            reset();
+        return;
+        }
+        else {
+            gameState = false;
+            return;
+        }
+    }
+
     function render() {
-        var rowImages = [
-                'images/water-block.png',
-                'images/stone-block.png',
-                'images/stone-block.png',
-                'images/stone-block.png',
-                'images/grass-block.png',
-                'images/grass-block.png'
-            ],
-            numRows = 6,
+            var numRows = 6,
             numCols = 5,
             row, col;
 
         for (row = 0; row < numRows; row++) {
             for (col = 0; col < numCols; col++) {
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                ctx.drawImage(Resources.get(SPRITES[background[row]]), col * 101, row * 83);
             }
         }
-
-        renderEntities();
+        if (gameState == true) {
+            renderEntities();
+        }
+        else {
+            renderSplitscreen();
+        }
     }
 
     function renderEntities() {
@@ -69,18 +110,46 @@ var Engine = (function(global) {
         player.render();
     }
 
+    function renderSplitscreen() {
+        ctx.beginPath();
+        ctx.lineWidth = "4";
+        ctx.strokeStyle = "green";
+        ctx.fillStyle = "white";
+        ctx.rect(103, 130, 303, 250);
+        ctx.stroke();
+        ctx.fill();
+        console.log(player.lives);
+        if (player.lives < 1) {
+            ctx.font="20px Georgia";
+            ctx.fillStyle = "green";
+            ctx.fillText("Game Over", 220, 220);
+            ctx.fillText("Press space to restart", 140, 240);
+       }
+    }
+
     function reset() {
-        // noop
+        player.x = 2 * 101;
+        player.y = 5 * 83 - 10;
+        allEnemies = [];
+        for (i = 0; i < randomNumber(1,10); i++) {
+            var enemy = new Enemy();
+        allEnemies.push(enemy);
+        }
     }
 
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        SPRITES['stone'],
+        SPRITES['water'],
+        SPRITES['grass'],
+        SPRITES['bug'],
+        SPRITES['boy'],
+        SPRITES['cat-girl'],
+        SPRITES['heart']
     ]);
     Resources.onReady(init);
 
+    global.canvas = canvas;
     global.ctx = ctx;
+    global.background = background;
+    global.SPRITES = SPRITES;
 })(this);
